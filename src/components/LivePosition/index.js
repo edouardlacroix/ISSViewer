d976f5afe3f7e15add024c2f6c04a52854ad5358
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-05-03 18:30:42 +0200

    Window for live position

--- src/components/LivePosition/index.js
+++ src/components/LivePosition/index.js
@@ -1,6 +1,28 @@
 import './styles.scss';
+import WinBox from "winbox/src/js/winbox";
+import "winbox/dist/css/winbox.min.css";
+import { useState } from 'react';
 
 function LivePosition() {
+    const [isVisible, setIsVisible] = useState(true)
+
+    const createWindow = () => {
+        setIsVisible(false)
+        new WinBox({
+            title: "ISS Live Position",
+            x: "center",
+            y: "center",
+            width: "30%",
+            height: "38%",
+            mount: document.getElementById("content")
+                .cloneNode(true),
+            border: "0.2em",
+            class: ["no-full"],
+            onclose: () => setIsVisible(true)
+
+        });
+    }
+
 
     return (
         <div className="livePositionWrapper">
@@ -12,7 +34,23 @@ function LivePosition() {
                 width="100%"
                 scrolling="no"
                 src={'https://isstracker.spaceflight.esa.int'}
+                style={{ display: isVisible ? 'block' : 'none' }}
             />
+
+            <div id="hiddenContent" style={{ display: "none" }}>
+                <div id="content">
+                    <iframe
+                        id="issLivePositionIframe"
+                        title="ISS Live Position"
+                        frameborder="0"
+                        height="100%"
+                        width="100%"
+                        scrolling="no"
+                        src={'https://isstracker.spaceflight.esa.int'}
+                    />
+                </div>
+            </div>
+            <div onClick={() => createWindow()} className={'openWindowWrapper'}> <p>{'[ ]'}</p></div>
         </div>
     );
 }
