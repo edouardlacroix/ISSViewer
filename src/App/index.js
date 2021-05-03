b96c41096ace3604c29c77e0ee9650159e43c02b
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-05-03 16:49:58 +0200

    Autoplay

--- src/App/index.js
+++ src/App/index.js
@@ -19,7 +19,7 @@ function App() {
           frameborder="0"
           height="100%"
           width="100%"
-          src={'https://ustream.tv/embed/17074538'}
+          src={'https://ustream.tv/embed/17074538?autoplay=true&controls=false&showtitle=false&html5ui=0'}
         />
       </div>
       <div className="livePositionWrapper">
